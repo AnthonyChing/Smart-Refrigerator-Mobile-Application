4c7a1d04b680b6298c2320d2319c0f6e76c47c97
AnthonyChing <75062396+AnthonyChing@users.noreply.github.com>
2022-07-15 16:57:06 +0800

    Delete get_Ingredient.py

--- get_Ingredient.py
+++
@@ -1,9 +0,0 @@
-def get_ingredient():
-    text = []
-    while True:
-        ingredient = input('Input ingredient. If finished, type " end ": ')
-        if ingredient == "end":
-            break
-        text.append(ingredient)
-    return text
-# don't need this anymore
