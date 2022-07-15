c5a0036ad25fa7a90e8ec53b79cdae0703e21432
AnthonyChing <75062396+AnthonyChing@users.noreply.github.com>
2022-07-15 16:57:04 +0800

    Delete dbconnect.py

--- dbconnect.py
+++
@@ -1,5 +0,0 @@
-import pyodbc
-conn = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};'
-                      r'DBQ=C:\Users\achin\Documents\Database\test.accdb'
-                      )
-cursor = conn.cursor()
